40c1257bd188c4b5b63d1c26f63a5aa45fe1d90b
Agsha Athalla Nurkareem <agshaathalla@gmail.com>
2023-03-20 10:34:35 +0700

    add function read file

---
+++ src/Main/Program.cs
@@ -0,0 +1,21 @@
+﻿using bacaFile;
+using System.Data;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string filename = "C:\\Users\\Asus\\Downloads\\New folder\\read txt\\read txt\\K R R R.txt";
+        TXT abc = new TXT();
+        abc.toMatrix(filename);
+        Console.WriteLine("done!"); Console.ReadLine();
+        Console.WriteLine();
+        
+    }
+}
---
+++ src/Main/bacaFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace bacaFile;
+public class TXT{
+	public void toMatrix(string filename)
+	{
+        String input = File.ReadAllText(filename);
+        int i = 0, j = 0;
+        string[] data = File.ReadAllLines(filename);
+        char[] chars = new char[data.Length];
+        int baris = data.Length;
+        int kolom = data[0].Split(' ').Length;
+
+        Console.WriteLine("baris: " + baris);
+        Console.WriteLine("kolom: " + kolom);
+
+        char[,] result = new char[baris, kolom];
+        foreach (var row in input.Split('\n'))
+        {
+            j = 0;
+            foreach (var col in row.Trim().Split(' '))
+            {
+                result[i, j] = char.Parse(col.Trim());
+                j++;
+            }
+            i++;
+        }
+
+        for (int w = 0; w < baris; w++)
+        {
+            for (int h = 0; h < kolom; h++)
+            {
+                Console.Write(result[w, h] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
