14c2eaeafc55df4b4bbff71421cdef521294e600
syrifaa <13521018@std.stei.itb.ac.id>
2023-03-24 18:59:33 +0700

    update path dfs

--- src/Main/DFS.cs
+++ src/Main/DFS.cs
@@ -4,67 +4,64 @@ using System.Drawing;
 
 namespace dfsSearching
 {
-    internal class Programs
+    internal class Program
     {
         static void Main(string[] args)
         {
             //char[,] arr = {{'K','R','R','R'}, {'X','R','X','X'}, {'X','T','X','X'}, {'X','R','R','T'}};
             //char[,] arr = { { 'K', 'R', 'R', 'R' }, { 'X', 'R', 'X', 'R' }, { 'X', 'R', 'X', 'R' }, { 'X', 'R', 'R', 'T' } };
-            char[,] arr = { { 'T', 'R', 'R', 'T', 'R', 'R', 'X' }, { 'X', 'R', 'X', 'R', 'X', 'X', 'X' }, { 'X', 'R', 'R', 'T', 'R', 'R', 'X' }, { 'X', 'R', 'X', 'X', 'X', 'X', 'X' }, { 'X', 'R', 'X', 'X', 'R', 'X', 'X' }, { 'X', 'R', 'X', 'X', 'R', 'X', 'X' }, { 'K', 'R', 'R', 'R', 'R', 'R', 'R' } };
+            //char[,] arr = { { 'T', 'R', 'R', 'T', 'R', 'R', 'X' }, { 'X', 'R', 'X', 'R', 'X', 'X', 'X' }, { 'X', 'R', 'R', 'T', 'R', 'R', 'X' }, { 'X', 'R', 'X', 'X', 'X', 'X', 'X' }, { 'X', 'R', 'X', 'X', 'R', 'X', 'X' }, { 'X', 'R', 'X', 'X', 'R', 'X', 'X' }, { 'K', 'R', 'R', 'R', 'R', 'R', 'R' } };
             //char[,] arr = { { 'K', 'R', 'R', 'R', 'R', 'R', 'X' }, { 'X', 'R', 'X', 'T', 'X', 'R', 'R' }, { 'X', 'T', 'X', 'R', 'X', 'R', 'X' }, { 'X', 'R', 'X', 'X', 'X', 'T', 'X' } };
             //char[,] arr = { { 'K', 'R', 'R', 'X', 'X', 'X' }, { 'X', 'X', 'R', 'R', 'R', 'T' }, { 'T', 'R', 'R', 'X', 'X', 'R' }, { 'R', 'X', 'R', 'R', 'R', 'R' } };
             //char[,] arr = { { 'K', 'X', 'X', 'X', 'X', 'X' }, { 'R', 'R', 'X', 'X', 'X', 'X' }, { 'R', 'R', 'R', 'X', 'X', 'X' }, { 'R', 'R', 'R', 'R', 'X', 'X' }, { 'R', 'R', 'R', 'R', 'R', 'X' }, { 'R', 'R', 'R', 'R', 'R', 'T' } };
             //char[,] arr = { { 'X', 'X', 'X', 'X', 'X', 'X' }, { 'X', 'T', 'K', 'R', 'T', 'X' }, { 'X', 'X', 'X', 'X', 'X', 'X' } };
-            //char[,] arr = TXT.toMatrix("TextFile1.txt");
+            char[,] arr = TXT.toMatrix("TextFile4.txt");
             Algorithm.printMatrix(arr);
-            Stack<Points> treasure = new Stack<Points>();
+            Stack<Point> treasure = new Stack<Point>();
             treasure = Algorithm.stackTreasure(arr);
             Algorithm.printStack(treasure);
-            Points K =  Algorithm.findK(arr);
-            Stack<Points> visited = new Stack<Points>();
+            Point K = Algorithm.findK(arr);
+            Stack<Point> visited = new Stack<Point>();
+            Stack<Point> path = new Stack<Point>();
             visited.Push(K);
+            path.Push(K);
             Algorithm.printStack(visited);
-            Stack<Points> unvisited = new Stack<Points>();
-            Stack<Points> backtracking = new Stack<Points>();
-            Stack<int> countNode = new Stack<int>();
-            int countBacktracking = 0;
+            Stack<Point> unvisited = new Stack<Point>();
             while (treasure.Count != 0)
             {
                 Console.WriteLine("Visited sekarang: " + visited.Peek().x + "," + visited.Peek().y);
                 Algorithm.treasureArrived(treasure, visited.Peek());
-                Console.WriteLine("Treasure skrg bgt");
-                Algorithm.printStack(treasure);
-                countBacktracking = Algorithm.adjacentPoints(arr, visited, unvisited, backtracking, countBacktracking, countNode, treasure);
+                Algorithm.adjacentPoint(arr, visited, unvisited);
                 if (treasure.Count != 0)
                 {
-                    Algorithm.move(visited, unvisited);
+                    Algorithm.move(visited, unvisited, path);
                 }
             }
             Console.WriteLine("Jalannya lewat mana sihhh");
-            Algorithm.printStack(visited);
-            Stack<Points> visitedCopy = visited;
-            Points[] array = visitedCopy.ToArray();
+            Algorithm.printStack(path);
+            Stack<Point> pathCopy = path;
+            Point[] array = pathCopy.ToArray();
             Algorithm.printArray(array);
-            int count = visited.Count;
-            string direction = Algorithm.printStep(visited, count);
+            int count = path.Count;
+            string direction = Algorithm.printStep(path, count);
             Console.WriteLine(direction);
             Console.WriteLine();
-            Console.WriteLine("Jumlah node yang dilewati: " + Algorithm.countNode(visited));
+            Console.WriteLine("Jumlah node yang dilewati: " + Algorithm.countNode(path));
             Console.ReadLine();
         }
     }
 
-    public class Points
+    public class Point
     {
         public int x { get; set; }
         public int y { get; set; }
 
-        public Points()
+        public Point()
         {
             x = 0;
             y = 0;
         }
-        public Points(int x, int y)
+        public Point(int x, int y)
         {
             this.x = x;
             this.y = y;
@@ -76,7 +73,7 @@ namespace dfsSearching
                 return false;
             }
 
-            Points temp = (Points)obj;
+            Point temp = (Point)obj;
             return (x == temp.x) && (y == temp.y);
         }
         public override int GetHashCode()
@@ -87,55 +84,55 @@ namespace dfsSearching
 
     public class Algorithm
     {
-        public static Stack<Points> stackTreasure(char[,] arr)
+        public static Stack<Point> stackTreasure(char[,] arr)
         {
-            Stack<Points> treasure = new Stack<Points>();
+            Stack<Point> treasure = new Stack<Point>();
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
                     if (arr[i, j] == 'T')
                     {
-                        treasure.Push(new Points(i, j));
+                        treasure.Push(new Point(i, j));
                     }
                 }
             }
             return treasure;
         }
-        public static string printStep(Stack<Points> s, int count)
+        public static string printStep(Stack<Point> s, int count)
         {
             string direction;
             if (s.Count == 1)
                 return "";
-            Points temp = s.Peek();
+            Point temp = s.Peek();
             s.Pop();
-            Points temp1 = s.Peek();
+            Point temp1 = s.Peek();
             direction = printStep(s, count) + "" + checkAdjacent(temp1, temp);
             s.Push(temp);
             return direction;
         }
-        public static void printStack(Stack<Points> s)
+        public static void printStack(Stack<Point> s)
         {
             if (s.Count == 0)
                 return;
-            Points temp = s.Peek();
+            Point temp = s.Peek();
             s.Pop();
             printStack(s);
             Console.WriteLine(temp.x + "," + temp.y);
             s.Push(temp);
         }
-        public static void printArray(Points[] a)
+        public static void printArray(Point[] a)
         {
             for (int i = 0; i < a.Length; i++)
             {
                 Console.WriteLine("(" + a[i].x + "," + a[i].y + ")");
             }
         }
-        public static void treasureArrived(Stack<Points> s, Points p)
+        public static void treasureArrived(Stack<Point> s, Point p)
         {
             if (s.Count == 0)
                 return;
-            Points temp = s.Peek();
+            Point temp = s.Peek();
             s.Pop();
             treasureArrived(s, p);
             if ((p.x != temp.x) | (p.y != temp.y))
@@ -154,109 +151,78 @@ namespace dfsSearching
                 Console.WriteLine();
             }
         }
-        public static Points findK(char[,] arr)
+        public static Point findK(char[,] arr)
         {
-            Points p = new Points();
+            Point p = new Point();
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
                     if (arr[i, j] == 'K')
                     {
-                        p = new Points(i, j);
+                        p = new Point(i, j);
                     }
                 }
             }
             return p;
         }
-        public static int adjacentPoints(char[,] arr, Stack<Points> visited, Stack<Points> unvisited, Stack<Points> backtracking, int countBacktracking, Stack<int> countNode, Stack<Points> treasure)
+        public static void adjacentPoint(char[,] arr, Stack<Point> visited, Stack<Point> unvisited)
         {
             int count = 0;
-            Points p = visited.Peek();
-            if ((p.x != 0) && (arr[p.x - 1, p.y] != 'X') && (!visited.Contains(new Points(p.x - 1, p.y))))
+            Point p = visited.Peek();
+            if ((p.x != 0) && (arr[p.x - 1, p.y] != 'X') && (!visited.Contains(new Point(p.x - 1, p.y))))
             {
-                Points p1 = new Points(p.x - 1, p.y);
+                Point p1 = new Point(p.x - 1, p.y);
                 unvisited.Push(p1);
                 count++;
             }
-            if ((p.y != 0) && (arr[p.x, p.y - 1] != 'X') && (!visited.Contains(new Points(p.x, p.y - 1))))
+            if ((p.y != 0) && (arr[p.x, p.y - 1] != 'X') && (!visited.Contains(new Point(p.x, p.y - 1))))
             {
-                Points p1 = new Points(p.x, p.y - 1);
+                Point p1 = new Point(p.x, p.y - 1);
                 unvisited.Push(p1);
                 count++;
             }
-            if ((p.x != arr.GetLength(0) - 1) && (arr[p.x + 1, p.y] != 'X') && (!visited.Contains(new Points(p.x + 1, p.y))))
+            if ((p.x != arr.GetLength(0) - 1) && (arr[p.x + 1, p.y] != 'X') && (!visited.Contains(new Point(p.x + 1, p.y))))
             {
-                Points p1 = new Points(p.x + 1, p.y);
+                Point p1 = new Point(p.x + 1, p.y);
                 unvisited.Push(p1);
                 count++;
             }
-            if ((p.y != arr.GetLength(1) - 1) && (arr[p.x, p.y + 1] != 'X') && (!visited.Contains(new Points(p.x, p.y + 1))))
+            if ((p.y != arr.GetLength(1) - 1) && (arr[p.x, p.y + 1] != 'X') && (!visited.Contains(new Point(p.x, p.y + 1))))
             {
-                Points p1 = new Points(p.x, p.y + 1);
+                Point p1 = new Point(p.x, p.y + 1);
                 unvisited.Push(p1);
                 count++;
             }
-            if (backtracking.Count != 0 && count != 0)
+            if (count == 0)
             {
-                countBacktracking++;
-                backtracking.Push(p);
+                Console.WriteLine("BackTracking");
             }
-            if (count > 1)
-            {
-                if (countBacktracking != 0)
-                {
-                    countNode.Push(countBacktracking);
-                }
-                Console.WriteLine("Simpangan");
-                countBacktracking = 1;
-                backtracking.Push(p);
-
-            }
-            Algorithm.treasureArrived(treasure, visited.Peek());
-            if (treasure.Count != 0)
-            {
-                if (count == 0)
-                {
-                    if (countBacktracking != 0)
-                    {
-                        countNode.Push(countBacktracking);
-                        int tempCount = countNode.Pop();
-                        while (tempCount != 0)
-                        {
-                            Points temp = backtracking.Pop();
-                            visited.Push(temp);
-                            tempCount--;
-                        }
-                        if (backtracking.Count != 0)
-                        {
-                            Points temp = visited.Peek();
-                            Points temp1 = backtracking.Peek();
-                            if (checkAdjacentBool(temp, temp1))
-                            {
-                                int tempCount1 = countNode.Pop();
-                                while (tempCount1 != 0)
-                                {
-                                    Points temp2 = backtracking.Pop();
-                                    visited.Push(temp2);
-                                    tempCount1--;
-                                }
-                            }
-                        }
-                    }
-                    Console.WriteLine("Backtraking nih");
-                    countBacktracking = 0;
-                }
-            }
-            return countBacktracking;
         }
-
-        public static void move(Stack<Points> visited, Stack<Points> unvisited)
+        public static void move(Stack<Point> visited, Stack<Point> unvisited, Stack<Point> path)
         {
-            Points temp = unvisited.Pop();
+            Point temp = unvisited.Pop();
+            Stack<Point> visitedCopy = new Stack<Point> (visited);
+            Reverse(visitedCopy);
+            if (!checkAdjacentBool(temp, visited.Peek()))
+            {
+                Console.WriteLine("masuk sini");
+                Point temp1 = visitedCopy.Pop();
+                Point temp2 = visitedCopy.Pop();
+                Point temp4 = visited.Pop();
+                path.Push(temp2);
+                Console.WriteLine("Temp: " + temp.x + "," + temp.y);
+                while (!checkAdjacentBool(temp, temp2))
+                {
+                    temp2 = visitedCopy.Pop();
+                    temp4 = visited.Pop();
+                    path.Push(temp2);
+                }
+            }
             visited.Push(temp);
+            path.Push(temp);
         }
-        public static string checkAdjacent(Points p1, Points p2)
+        public static string checkAdjacent(Point p1, Point p2)
         {
             string direction;
             if (p1.x == p2.x + 1 && p1.y == p2.y)
@@ -271,7 +237,7 @@ namespace dfsSearching
                 direction = "Backtracking sampai di (" + p2.x + "," + p2.y + ")";
             return direction;
         }
-        public static bool checkAdjacentBool(Points p1, Points p2)
+        public static bool checkAdjacentBool(Point p1, Point p2)
         {
             bool flag = false;
             if (p1.x == p2.x + 1 && p1.y == p2.y)
@@ -284,16 +250,35 @@ namespace dfsSearching
                 flag = true;
             return flag;
         }
-        public static int countNode(Stack<Points> s)
+        public static int countNode(Stack<Point> s)
         {
             if (s.Count == 0)
                 return 0;
-            Points temp = s.Peek();
+            Point temp = s.Peek();
             s.Pop();
             countNode(s);
             if (!s.Contains(temp))
                 s.Push(temp);
             return s.Count;
+        }
+        static void Reverse(Stack<Point> s)
+        {
+            if (s.Count == 0)
+                return;
+            Point element = s.Pop();
+            Reverse(s);
+            InsertAndRearrange(s, element);
+        }
+        static void InsertAndRearrange(Stack<Point> s, Point element)
+        {
+            if (s.Count == 0)
+                s.Push(element);
+            else
+            {
+                Point temp = s.Pop();
+                InsertAndRearrange(s, element);
+                s.Push(temp);
+            }
         }
     }
 }
